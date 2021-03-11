158f9f7e81d8005703e138c14e1f4a8ef7e43d47
EthanCooper20 <escoope77@gmail.com>
2021-03-11 13:11:48 -0600

    Added Music And Modifying Game

---
+++ aa-Replica/Assets/Scripts/gamecontrol.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gamecontrol : MonoBehaviour
+{
+    public static gamecontrol control;
+
+    public Text score;
+    public int scorevalue;
+    public Text lives;
+    public Text playername;
+    public int currentlives;
+    public InputField inputname;
+    public string stringofname;
+
+
+
+    void Awake()
+    {
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+
+
+
+    }
+    //gamecontrol.control.currentlives
+}
